0c1d724d13a5aef02f3be5ba681423c957d593dd
Fauzank216 <xyzan3585@gmail.com>
2025-11-30 21:48:07 +0700

    Update organisasi (1).sql

--- organisasi (1).sql
+++ organisasi (1).sql
@@ -75,6 +75,7 @@ CREATE TABLE `schedule` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `date_start` date NOT NULL,
   `date_end` date DEFAULT NULL,
+   `avatar` text NOT NULL DEFAULT '/profile.webp',
   `status` enum('Toward','Done','Cancel') NOT NULL DEFAULT 'Toward'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
